bdf52d7dfa0ef9159c5e3f83ad1ff451034f4243
unknown <milosmi11166@gmail.com>
2016-07-10 21:27:07 +0200

    Slike i putanje u bazi

--- src/db/GiftShopInsert.sql
+++ src/db/GiftShopInsert.sql
@@ -9,5 +9,5 @@ insert into giftshop.user
 values (1, "pera@gmail.com", "AFSDF@$#OJFASDH@#$@O#PDQSDA455356afsdfSDF#@$@QSADFAFVCVzxcxclfg", "Petar Petrović", "Strahinjića Bana 133, Beograd", "0651659751", now(), 2);
 
 insert into giftshop.gift
-values (1, "Samsung Galaxy s7", "Poklanjam Samsung Galaxy s7", null, null, null, true, 2, 1),
-(2, "Jakna Zara", "Poklanjam Jakna Zara", null, null, null, true, 1, 1);
+values (1, "Samsung Galaxy s7", "Poklanjam Samsung Galaxy s7", "img/samsungs7.jpg", null, null, true, 2, 1),
+(2, "Jakna Zara", "Poklanjam Jakna Zara", "img/zarajacket.jpg", null, null, true, 1, 1);
